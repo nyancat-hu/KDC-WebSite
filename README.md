51a75cf32e75c6f5b51795a4fc837fea2de03cc8
他是身经百战了 <2416510131@qq.com>
2022-08-10 21:31:47 +0800

    新增联机下载客户端功能，兼用华为接口

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
 # KDC-WebSite
 口袋村服务器的Web仓库
 
-**在线更新功能注意事项：**
+### **在线更新功能注意事项：**
 
 - 更新之前最好先自己试一下
 
-- 修改服务端的version.yaml
+##### -）更新步骤如下
 
-    - 注意修改当前版本
-    - 注意修改上次版本
-    - 注意修改下载地址
+- help/update接口需包含的更新包
 
-- 修改服务端的NsisoLauncher.zip
+  - **minecraft-Update.zip**
 
-    - 将version.yaml替换进去
+  - **NsisoLauncher.zip**(记得放入最新的version.yaml)
 
-- 必须先删除文件再放入文件，才会有效果
+  - **minecraft-Full.zip**(JavaZulu文件夹必须为空)
 
-  **默认不会进行文件覆盖**
+    ↑上述文件每次更新都需要重新上传↑
+
+  - Jre_Win32.zip
+
+  - Jre_Win64.zip
+
+  - Jre_MACx64.zip
+
+  - Jre_MACarm.zip
+
+- 修改version.yaml文件，记得新旧版本号递进
+
+    version.yaml中必须先删除文件再放入文件，才会有效果
--- src/main/java/top/imwonder/mcauth/controller/api/HelpController.java
+++ src/main/java/top/imwonder/mcauth/controller/api/HelpController.java
@@ -9,12 +9,13 @@ import top.imwonder.mcauth.pojo.responsebody.ApiMeta;
 import top.imwonder.mcauth.pojo.responsebody.DownloadBean;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-
 
 @RestController
 @RequestMapping("/help")
 public class HelpController {
+
 
     @GetMapping("/status")
     @ResponseStatus(code = HttpStatus.OK, reason = "Successful!")
@@ -23,11 +24,11 @@ public class HelpController {
     }
 
     @GetMapping("/update")
-    public DownloadBean getFileUrl(String FileName) throws UnsupportedEncodingException {
+    public DownloadBean getFileUrl(String FileName) {
         final DownloadBean downloadBean = new DownloadBean();
         downloadBean.setUrl(String.format("http://117.78.1.88/source/home:jhu/KDC-resource/%s", FileName));
         // 返回登录信息的转码
-        downloadBean.setAuth("Basic " + Base64.getEncoder().encodeToString("jhu:430525@HJf".getBytes("utf-8")));
+        downloadBean.setAuth("Basic " + Base64.getEncoder().encodeToString("jhu:430525@HJf".getBytes(StandardCharsets.UTF_8)));
         return downloadBean;
     }
 
